a89598499daa6f1b26789a3d8c87672513703beb
Soban Farooq <sobanfarooq3@gmail.com>
2023-07-15 21:49:46 +0500

    added input meta deta

--- src/composables/useInput.js
+++ src/composables/useInput.js
@@ -2,15 +2,27 @@ export function useInput() {
     const inputs = {
         simpleInput: {
             label: 'Input',
-            icon: 'dynamic_form'
+            icon: 'dynamic_form',
+            code: `<input type='text' name='##name##' placeholder='##placeholder##' v-model="##v-model##" class='mb-4 w-full text-sm shadow-sm border rounded-md px-3 py-2 placeholder:text-gray-300 placeholder:text-sm focus:outline-8 focus:outline-blue-600'  />`,
+            inputName: '',
+            inputValue: '',
+            inputPlaceholder: '',
         },
         email: {
             label: 'Email',
-            icon: 'mail'
+            icon: 'mail',
+            code: `<input type='email' name='##name##' placeholder='##placeholder##' v-model="##v-model##" class='mb-4 w-full text-sm shadow-sm border rounded-md px-3 py-2 placeholder:text-gray-300 placeholder:text-sm focus:outline-8 focus:outline-blue-600'  />`,
+            inputName: '',
+            inputValue: '',
+            inputPlaceholder: '',
         },
         password: {
             label: 'Password',
-            icon: 'password'
+            icon: 'password',
+            code: `<input type='password' name='##name##' placeholder='##placeholder##' v-model="##v-model##" class='mb-4 w-full text-sm shadow-sm border rounded-md px-3 py-2 placeholder:text-gray-300 placeholder:text-sm focus:outline-8 focus:outline-blue-600'  />`,
+            inputName: '',
+            inputValue: '',
+            inputPlaceholder: '',
         },
     }
 
